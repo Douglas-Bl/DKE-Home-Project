fabd4886c6d1a1235d8fa8bd3ed0819500d70ab0
Douglas-Bl <Douglas.Blank@hhu.de>
2023-01-09 10:56:46 +0100

    removed unnecessary imports and add requirements.txt

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,29 +1,32 @@
-import os
-
 from SPARQLWrapper import SPARQLWrapper, JSON
-
 from eval.eval import evaluate
 from src.main.python.classifier import create_classifier
 from src.main.python.test_classifier import test_classification
 
 
-sparql = SPARQLWrapper(
-        "https://data.gesis.org/claimskg/sparql"
-    )
-sparql.setReturnFormat(JSON)
+def main():
+
+    sparql = SPARQLWrapper(
+            "https://data.gesis.org/claimskg/sparql"
+        )
+    sparql.setReturnFormat(JSON)
 
 
-# create classifier and get dicts with author and mention score
-clf, authors, mentions = create_classifier(sparql)
+    # create classifier and get dicts with author and mention score
+    clf, authors, mentions = create_classifier(sparql)
 
-# save classifier
-#dump(clf, 'decision_tree.joblib')
+    # save classifier
+    #dump(clf, 'decision_tree.joblib')
 
-# load saved classifier
-#clf = load('decision_tree.joblib')
+    # load saved classifier
+    #clf = load('decision_tree.joblib')
 
-# write predicted ratings of test set into output_data
-test_classification(sparql, clf, authors, mentions)
+    # write predicted ratings of test set into output_data
+    test_classification(sparql, clf, authors, mentions)
 
-# evaluate classification on test data
-evaluate()
+    # evaluate classification on test data
+    evaluate()
+
+
+if __name__ == "__main__":
+    main()
